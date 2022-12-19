31c26e45cb2120fbad3a79ae3bfb9ccf8fa890b1
IrinaNeu <120553867+IrinaNeu@users.noreply.github.com>
2022-12-19 17:07:39 +0700

    домашнее задание

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 10 Написать программу, которая принимает на вход трехзначное число
+//и показывает вторую цифру этого  числа. 
+
+// int num = new Random().Next(100,1000);
+
+// Console.WriteLine(num);
+
+// int num1 = num/10;
+// int num2 =num1%10;
+
+// Console.WriteLine(num2);
+
+//_________________________________________
+
+// Задача 13 Написать программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32 (Console.ReadLine());
+
+// if (number1>99)
+// {
+//     for (; number1>=1000; number1/=10); 
+//   int  Result = number1%10;
+//     Console.WriteLine(Result);
+
+// }
+// else 
+// {
+//     Console.WriteLine("третьей цифры нет");
+
+// }
+
+// //____________________________________________________
+
+// Напишите программу , которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.  
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32 (Console.ReadLine());
+
+// if((number1==1)||(number1==2)||(number1==3)||(number1==4)||(number1==5))
+// {
+//      Console.WriteLine("нет");
+
+// }
+
+// else if((number1==6)||(number1==7))
+// {
+//     Console.WriteLine("да");
+// }
+
+    
